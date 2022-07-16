e1283a0c939fb293e88f63a39c353fc25aa06cb8
Ulrich Santos <102335999+ulrichsantos@users.noreply.github.com>
2022-07-16 14:57:17 -0300

    Netflix clone
    Work progress

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, { useEffect, useState } from 'react';
+import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
-
+  const [featuredData, setFeaturedData] = useState(null);
 
   useEffect(()=>{
     const loadAll = async () => {
       // Pegando a lista TOTAL DE FILMES
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      // Pegando o Featured
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.lenght -1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
     }
 
     loadAll();
@@ -19,6 +28,11 @@ export default () => {
 
   return (
     <div className='page'>
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />
+      }
+
       <section className='lists'>
         {movieList.map((item, key)=>(
         <MovieRow key={key} title={item.title} items={item.items} />
